5534b5898d86225b37e136ba3436d909357dab13
yumaf8284 <yuma.yf425@gmail.com>
2023-02-10 17:18:58 +0900

    クローン

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -18,7 +18,7 @@ class FavoritesController < ApplicationController
       def destroy
         @favorite = Favorite.find_by(user_id: current_user.id, topic_id: params[:topic_id])
         @favorite.destroy
-        redirect_to topics_path, success: 'いいねを取り消しました'
+        redirect_to topics_path, success: 'お気に入りを取り消しました'
       end
 
     end
